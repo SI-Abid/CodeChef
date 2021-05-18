2cfa141a8780199600c2ad19322442b4be8452f1
SI-Abid <siaaid5323@gmai.com>
2021-05-18 17:40:46 +0600

    long contest

---
+++ aprilcookoff/PSGRADE.cpp
@@ -0,0 +1,47 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define be begin()
+#define en end()
+#define all(x) (x).begin(),(x).end()
+#define out cout<<setprecision(20)
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int main(int argc, char* argv[])
+{
+    if(argc == 2 or argc == 3) freopen(argv[1], "r", stdin);
+    if(argc == 3) freopen(argv[2], "w", stdout);
+    ios::sync_with_stdio(false);
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int aa, bb, cc, t, a, b, c;
+        cin>>aa>>bb>>cc>>t>>a>>b>>c;
+        if(a<aa || b<bb || c<cc || a+b+c<t)
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+        }
+    }
+    
+    return 0;
+}
