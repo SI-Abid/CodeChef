de9a26b46f03b1acd7537c3afc73b6140429eeab
Saiham Islam <siaabid5323@gmail.com>
2021-08-02 18:55:21 +0600

    short contest

---
+++ JULY21/shoefit.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define be begin()
+#define en end()
+#define rb rbegin()
+#define re rend()
+#define all(x) (x).begin(),(x).end()
+#define out cout<<setprecision(20)
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+int main(int argc, char* argv[])
+{
+    if(argc == 2 or argc == 3) freopen(argv[1], "r", stdin);
+    if(argc == 3) freopen(argv[2], "w", stdout);
+    ios::sync_with_stdio(false);
+
+    int _; cin >> _;
+    while(_--)
+    {
+        int a=0,b;
+        for(int i=0;i<=2;i++)
+        {
+            cin >> b;
+            a += b;
+        }
+        if(a>0 and a<3) cout << "1\n";
+        else cout << "0\n";
+    }
+
+    return 0;
+}
